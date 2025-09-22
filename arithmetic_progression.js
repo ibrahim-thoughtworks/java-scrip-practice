8c83fbcfe7b3769403316e3ac6e6c482e0428e03
ibrahim-thoughtworks <mydrive1a@gmail.com>
2025-09-22 09:37:09 +0530

    created arithmetic progression js code

---
+++ arithmetic_progression.js
@@ -0,0 +1,12 @@
+const input =  15;
+let answer = 0;
+
+
+console.log("arithematic progression of",input);
+
+for (let iteration=1; iteration<=input; iteration++){
+    answer = answer + iteration;
+    console.log(answer);
+}
+
+
